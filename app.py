6b39362e08814945ed826f6fff3e09fe29be3c59
DIMMMs <152955234+SsstttDiam@users.noreply.github.com>
2025-06-11 16:28:35 +0700

    app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import numpy as np
 import joblib
 
 # Load model dan encoder
-model = joblib.load('hepatitis_model.pkl')
+model = joblib.load('river.pkl')
 label_encoder = joblib.load('label_encoder.pkl')
 
 st.title("Prediksi Kategori Pasien Hepatitis")
