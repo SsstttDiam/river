38c6b6751e060ea0f5a557b0712846d4e8bc681c
DIMMMs <152955234+SsstttDiam@users.noreply.github.com>
2025-06-11 16:41:10 +0700

    app.py

--- app.py
+++ app.py
@@ -7,7 +7,8 @@ model = joblib.load('river.pkl')
 label_encoder = joblib.load('label_encoder.pkl')
 
 st.title("Prediksi Kategori Pasien Hepatitis")
-st.markdown
+
+st.markdown("*Gunakan **titik (.)** sebagai pemisah desimal, bukan koma (,).*")
 
 # Form input
 with st.form("form_hepatitis"):
@@ -15,16 +16,16 @@ with st.form("form_hepatitis"):
 
     age = st.number_input("Usia", min_value=1, max_value=120)
     sex = st.selectbox("Jenis Kelamin", ["Laki-laki", "Perempuan"])
-    alb = st.number_input("Albumin (ALB)")
-    alp = st.number_input("Alkaline Phosphatase (ALP)")
-    alt = st.number_input("Alanine Aminotransferase (ALT)")
-    ast = st.number_input("Aspartate Aminotransferase (AST)")
-    bil = st.number_input("Bilirubin (BIL)")
-    che = st.number_input("Cholinesterase (CHE)")
-    chol = st.number_input("Kolesterol (CHOL)")
-    crea = st.number_input("Creatinine (CREA)")
-    ggt = st.number_input("GGT")
-    prot = st.number_input("Protein Total (PROT)")
+    alb = st.number_input("Albumin (ALB)", format="%.2f")
+    alp = st.number_input("Alkaline Phosphatase (ALP)", format="%.2f")
+    alt = st.number_input("Alanine Aminotransferase (ALT)", format="%.2f")
+    ast = st.number_input("Aspartate Aminotransferase (AST)", format="%.2f")
+    bil = st.number_input("Bilirubin (BIL)", format="%.2f")
+    che = st.number_input("Cholinesterase (CHE)", format="%.2f")
+    chol = st.number_input("Kolesterol (CHOL)", format="%.2f")
+    crea = st.number_input("Creatinine (CREA)", format="%.2f")
+    ggt = st.number_input("GGT", format="%.2f")
+    prot = st.number_input("Protein Total (PROT)", format="%.2f")
 
     submitted = st.form_submit_button("Prediksi")
 
